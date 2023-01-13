fd444a38066110889ef5103beabedb8225c58e98
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-01-13 19:25:51 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,138 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+let user = {
+  pLength: 0,
+  special: true,
+  number: true,
+  lower: true,
+  upper: true,
+}
+
+
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  prompt("How long would you like the password to be?");
+  confirm("Would you like ... in your passward?");
+  confirm("")
+
+} // should return something
+// can make an object with answers
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+} // yes should return. will return a single random element.
+
+// Function to generate password with user input
+function generatePassword() {
+  let password = 'xyz';
+
+  // logic goes here
+
+  return password
+
+} // uses above two functions to generate a password
+
+// do not need to worry about code below
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
