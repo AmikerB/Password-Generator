39131f7251d2eebac6ef9b15ec95a80ab089c615
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-01-16 17:11:11 +0000

    fix: code will not run if all variables are false

--- script.js
+++ script.js
@@ -88,72 +88,72 @@ let upperCasedCharacters = [
   'Z'
 ];
 
-let shouldUseLower = true;
-let shouldUseUpper = true;
-let shouldUseNumber = true;
-let shouldUseSpecial = true;
-let passwordCharacters = [];
 
-let howLong = prompt("How long would you like the password to be? (Must be between 10 and 64 characters)");
+function generatePassword() {
+  let shouldUseLower = true;
+  let shouldUseUpper = true;
+  let shouldUseNumber = true;
+  let shouldUseSpecial = true;
+  let passwordCharacters = [];
 
-// converting the string answer to a number so can be used later
-let lengthOfPassword = Number(howLong);
+  let howLong = prompt("How long would you like the password to be? (Must be between 10 and 64 characters)");
 
-// making sure the number is between 10 and 64 before the other questions are asked.
-if (lengthOfPassword >= 10 && lengthOfPassword <= 64) {
-  shouldUseLower = confirm("Would you liker lower cased characters in your password?")
-  shouldUseUpper = confirm("Would you like upper cased characters in your password?");
-  shouldUseNumber = confirm("Would you like numbers in your password?");
-  shouldUseSpecial = confirm("Would you like special characters in your password?");
-} else {
-  alert("Invalid password length. Please enter a number between 10 and 64.");
+  // converting the string answer to a number so can be used later
+  let lengthOfPassword = Number(howLong);
+
+  // making sure the number is between 10 and 64 before the other questions are asked.
+  if (lengthOfPassword >= 10 && lengthOfPassword <= 64) {
+    shouldUseLower = confirm("Would you liker lower cased characters in your password?")
+    shouldUseUpper = confirm("Would you like upper cased characters in your password?");
+    shouldUseNumber = confirm("Would you like numbers in your password?");
+    shouldUseSpecial = confirm("Would you like special characters in your password?");
+  } else {
+    alert("Invalid password length. Please enter a number between 10 and 64.");
+  }
+
+  // checking that user has selected atleast one of the variables as true
+  if (!shouldUseLower && !shouldUseUpper && !shouldUseNumber && !shouldUseSpecial) {
+    alert("At least one of the shouldUse* variable should be true");
+  } else {
+    //function to create an array of characters selected by user. Randomises the characters first then adds them to an array depending on how long the user wants the array to be. 
+    while (passwordCharacters.length < lengthOfPassword) {
+      if (shouldUseLower && passwordCharacters.length < lengthOfPassword) {
+        let indexOfLower = Math.floor(Math.random() * lowerCasedCharacters.length);
+        passwordCharacters.push(
+          lowerCasedCharacters[indexOfLower]
+        );
+      }
+      if (shouldUseUpper && passwordCharacters.length < lengthOfPassword) {
+        let indexOfUpper = Math.floor(Math.random() * upperCasedCharacters.length);
+        passwordCharacters.push(
+          upperCasedCharacters[indexOfUpper]
+        );
+      }
+      if (shouldUseNumber && passwordCharacters.length < lengthOfPassword) {
+        let indexOfNumber = Math.floor(Math.random() * numericCharacters.length);
+        passwordCharacters.push(
+          numericCharacters[indexOfNumber]
+        );
+      }
+      if (shouldUseSpecial && passwordCharacters.length < lengthOfPassword) {
+        let indexOfSpecial = Math.floor(Math.random() * specialCharacters.length);
+        passwordCharacters.push(
+          specialCharacters[indexOfSpecial]
+        );
+      }
+    }
+  }
+
+  // shuffle characters within the password characters array using the Fisher-Yates shuffle algorithm to create a new character order
+  for (let i = passwordCharacters.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [passwordCharacters[i], passwordCharacters[j]] = [passwordCharacters[j], passwordCharacters[i]];
+  }
+
+  // convert the password characters array into a single string
+  let passwordCharactersString = passwordCharacters.join('');
+  return passwordCharactersString;
 }
-
-if (!shouldUseLower && !shouldUseUpper && !shouldUseNumber && !shouldUseSpecial) {
-  alert("At least one of the shouldUse* variable should be true");
-}
-
-
-//function to create an array of characters selected by user. Randomises the characters first then adds them to an array depending on how long the user wants the array to be. 
-
-while (passwordCharacters.length < lengthOfPassword) {
-  if (shouldUseLower && passwordCharacters.length < lengthOfPassword) {
-    let indexOfLower = Math.floor(Math.random() * lowerCasedCharacters.length);
-    passwordCharacters.push(
-      lowerCasedCharacters[indexOfLower]
-    );
-  }
-  if (shouldUseUpper && passwordCharacters.length < lengthOfPassword) {
-    let indexOfUpper = Math.floor(Math.random() * upperCasedCharacters.length);
-    passwordCharacters.push(
-      upperCasedCharacters[indexOfUpper]
-    );
-  }
-  if (shouldUseNumber && passwordCharacters.length < lengthOfPassword) {
-    let indexOfNumber = Math.floor(Math.random() * numericCharacters.length);
-    passwordCharacters.push(
-      numericCharacters[indexOfNumber]
-    );
-  }
-  if (shouldUseSpecial && passwordCharacters.length < lengthOfPassword) {
-    let indexOfSpecial = Math.floor(Math.random() * specialCharacters.length);
-    passwordCharacters.push(
-      specialCharacters[indexOfSpecial]
-    );
-  }
-};
-
-// shuffle characters within the password characters array using the Fisher-Yates shuffle algorithm
-for (let i = passwordCharacters.length - 1; i > 0; i--) {
-  let j = Math.floor(Math.random() * (i + 1));
-  [passwordCharacters[i], passwordCharacters[j]] = [passwordCharacters[j], passwordCharacters[i]];
-}
-
-// join the password characters array into a string
-let passwordCharactersString = passwordCharacters.join('');
-console.log(passwordCharactersString);
-
-
 
 // // // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
